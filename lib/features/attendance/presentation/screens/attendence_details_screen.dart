e37d0d69b28b128b2fef61036c733c89bb3239fb
mayank <knowledgesmart24@gmail.com>
2026-08-24 03:54:14 +0530

    Add screens for attendance feature: Attendance, Attendance Details, Login, Splash, Chat, Home, Profile, and Timetable

---
+++ lib/features/attendance/presentation/screens/attendence_details_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class AttendenceDetailsScreen extends StatelessWidget {
+  const AttendenceDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Attendance Details'),
+      ),
+    );
+  }
+}
---
+++ lib/features/timetable/presentation/screens/timetable_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+class TimetableScreen extends StatelessWidget {
+  const TimetableScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Timetable'),
+      ),
+    );
+  }
+}
