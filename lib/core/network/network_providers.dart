9c59fd0cefca3068d8f861c1d1663b612f49bfce
mayank <knowledgesmart24@gmail.com>
2026-08-27 00:44:28 +0530

    Add authApiProvider and authRepositoryProvider for authentication integration

--- lib/core/network/network_providers.dart
+++ lib/core/network/network_providers.dart
@@ -1,9 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'api_client.dart';
+import '../../../core/network/api_client.dart';
+import '/features/auth/data/auth_api.dart';
+import '/features/auth/data/auth_repository.dart';
 
-final apiClientProvider = Provider<ApiClient>(
-  (ref) {
-    return ApiClient();
-  },
-);
+final authApiProvider = Provider<AuthApi>((ref) {
+  final dio = ref.watch(apiClientProvider);
+
+  return AuthApi(dio);
+});
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  final api = ref.watch(authApiProvider);
+
+  return AuthRepository(api);
+});
